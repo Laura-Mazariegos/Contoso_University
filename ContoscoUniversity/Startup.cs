9651b2fc0d34bb354a9f569c324c515de7c966b7
laura.mazariegos <35360118+Laura-Mazariegos@users.noreply.github.com>
2019-03-28 23:34:54 -0500

    initial startup

--- ContoscoUniversity/Startup.cs
+++ ContoscoUniversity/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ContoscoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContoscoUniversity
 {
@@ -24,6 +26,9 @@ namespace ContoscoUniversity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SchoolContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
