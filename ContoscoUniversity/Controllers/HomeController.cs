c9a40296e869bf43b4b7af6cb97fcd1ed0047b21
laura.mazariegos <35360118+Laura-Mazariegos@users.noreply.github.com>
2019-03-29 21:59:20 -0500

    part 3

--- ContoscoUniversity/Controllers/HomeController.cs
+++ ContoscoUniversity/Controllers/HomeController.cs
@@ -5,11 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContoscoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using ContoscoUniversity.Data;
+using ContoscoUniversity.Models.SchoolViewModels;
 
 namespace ContoscoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -24,6 +33,19 @@ namespace ContoscoUniversity.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
