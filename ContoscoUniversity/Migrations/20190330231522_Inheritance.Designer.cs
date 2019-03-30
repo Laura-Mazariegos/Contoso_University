ada877c26fffbb9b2f41d0e8876dabf14f4594df
laura.mazariegos <35360118+Laura-Mazariegos@users.noreply.github.com>
2019-03-30 18:20:52 -0500

    part 8

--- ContoscoUniversity/Migrations/20190330231522_Inheritance.Designer.cs
+++ ContoscoUniversity/Migrations/20190330231522_Inheritance.Designer.cs
@@ -4,14 +4,16 @@ using ContoscoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContoscoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20190330231522_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
